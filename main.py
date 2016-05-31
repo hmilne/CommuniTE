3e1c550f03c9b3e8f825e50741d625a022daf9d8
hmilne <hmilne@reed.edu>
2016-05-31 12:08:41 -0700

    Move Seq class to main

--- main.py
+++ main.py
@@ -80,6 +80,18 @@ logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
 
 ###### Parsing input fasta file ########
+class Seq:
+    def __init__(self, num, seqID):
+        self.num = num
+        self.ID = seqID
+        # self.sequence = sequence
+        self.attributes = {}
+    def setAttribute(self, key, value):
+        self.attributes[key] = value
+    def length(self):
+        return len(self.sequence)
+    def setSequence(self, sequence):
+        self.sequence = sequence
 
 
 def check_file_path(filename):
